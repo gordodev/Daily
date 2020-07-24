677f6f6571bb45d3e5ba28cc20f07ee72aa7bd5a
gordodev <carlylespeaks@gmail.com>
2020-07-24 01:37:45 -0400

    Trying to figure out how to restart loop to get 2nd element

--- randomCodeSnips/daily_searchNestList.py
+++ randomCodeSnips/daily_searchNestList.py
@@ -3,6 +3,7 @@
 #Application that will search for words in lists as well as in nexted lists
 #In this case the list elements represent skills
 
+import time
 
 #Create empty lists
 
@@ -10,14 +11,26 @@ mainList=[]
 subList=[]
 
 while True:
-    #get list name & if nothing entered break
-    skill=input("Enter UNIX synonym: \n")
-    if skill=='':             #Exist loop if nothing entered
-        print('DONE!')
+    if len(subList)!=0:
+        print('not')
+        print('You want another?')
+        time.sleep(2)
+    if len(subList)==0:
+        mainList.insert(0,subList)
+        reply=input('Add to list?\n')
+        if reply=="y" or reply=="Y":
+            while True:
+                #get list name & if nothing entered break
+                skill=input("Enter UNIX synonym: \n")
+                if skill=='':             #Exist loop if nothing entered
+                    print('DONE!')
+                    break
+                subList.insert(0,skill)   #Populate list UNIX=[inserts,inserts]
+        else:
+            print('nope\n')
+            break
+    else:
         break
-    subList.insert(0,skill)   #Populate list UNIX=[inserts,inserts]
-
-    
     #Split list items into seperate elements
 
     #insert into main list
