a52fb6335d55bc4380298fe265c5190ccdb5b26f
gordodev <carlylespeaks@gmail.com>
2020-07-28 00:01:38 -0400

    Convert hourly to yearly

---
+++ randomCodeSnips/daily_payConvertHr2Yearly.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Calculate yearly salary
+
+
+#Take hourly rate from user
+hourly=int(input("How much do you make hourly?\n"))
+
+#Calculate hourly and print
+yearly=(hourly*160*12)
+
+print (yearly)
+~                  
