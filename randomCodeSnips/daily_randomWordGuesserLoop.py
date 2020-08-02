8d7dd439e42a0835145b1b022a391fe8526ecdbc
gordodev <carlylespeaks@gmail.com>
2020-08-02 12:23:54 -0400

    changing word choser to function

---
+++ randomCodeSnips/daily_randomWordGuesserLoop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+#Picks random word form list, then tries to guess the word using loop
+
+import random
+
+words=[]
+
+def get_words():
+    while True:
+        word=input("Enter some words: \n")
+        if word == "":
+            break
+        #words=[]
+        words.insert(0,word)
+get_words()
+print ("You are done!\n[words]")
+print (random.choice(words))
+
+
+
