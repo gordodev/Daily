4f68c42405d59c89666aff8841f7a0e9ccf32638
gordodev <carlylespeaks@gmail.com>
2020-08-04 23:56:52 -0400

    adding draft fist one function

--- randomCodeSnips/daily_firstOne.py
+++ randomCodeSnips/daily_firstOne.py
@@ -6,15 +6,27 @@ import random
 import time
 
 numbers=[]
+numbersList=[]
 
 def random_list():
     numbers=random.randrange(1000000000000, 1999999999999)
     print (numbers)
+    global numbersList
     numbersList = [int(x) for x in str(numbers)]
-    print (numbers[0])
+    #return numbersList
+    #print ("numbersList in random function: ",numbersList)
 
+def first_one(num):              #Find first number
+    #print (numbersList)   
+    #print (num)
+    found=0
+    for x in num:
+        if x == 1:
+            found=1
 
 while True:
     print ("This is your random number")
     random_list()
+    print(numbersList)
+    first_one(numbersList)
     time.sleep(0.5)
