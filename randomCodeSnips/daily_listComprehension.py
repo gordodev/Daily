7fb40196b3248cc0eb4e097bb3e09569683eff6d
gordodev <carlylespeaks@gmail.com>
2020-08-13 23:51:35 -0400

    List comprehension

--- randomCodeSnips/daily_listComprehension.py
+++ randomCodeSnips/daily_listComprehension.py
@@ -13,6 +13,8 @@ print ("", output_list)
 
 
 input_list=[1,2,3,4,4,5,6,7,7]
-list_using_comp = [var for var in input_list if var % 2 == 0]
+list_using_comp = [var for var in input_list if var % 2 == 0 ]
+
+#list_using_comp = [var for var in input_list if var % 2 == 0 if var not in output_list]
 
 print ("We are here", list_using_comp)
