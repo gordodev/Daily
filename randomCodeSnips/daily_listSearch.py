5a5f4fd7523a30b03b8a11fa554911663a74d998
gordodev <carlylespeaks@gmail.com>
2020-08-14 23:59:16 -0400

    List search

--- randomCodeSnips/daily_listSearch.py
+++ randomCodeSnips/daily_listSearch.py
@@ -3,13 +3,13 @@
 #Search a list and find item
 
 
-words = input("Enter you list: \n")
-myList = words.split(' ')
-
-print (myList[1])
-
-searchWord = input("What word you need?\n")
+words = input("Enter a list of words: \n")
+myList = words.split(' ')  #Split words by space
 
 
+searchWord = input("What word you need?\n") #get search word
+
+index = myList.index(searchWord) #Identify the index of word in question
+print (index)
 
 
