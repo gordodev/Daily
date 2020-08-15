467b516dc51efaeba34e323039618292482ff2cf
gordodev <carlylespeaks@gmail.com>
2020-08-14 23:54:04 -0400

    List search

---
+++ randomCodeSnips/daily_listSearch.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#Search a list and find item
+
+
+words = input("Enter you list: \n")
+myList = words.split(' ')
+
+print (myList[1])
+
+searchWord = input("What word you need?\n")
+
+
+
+
