8c87246e784c621aec9f8f580421636991675f9c
gordodev <carlylespeaks@gmail.com>
2020-10-15 00:01:59 -0400

    Epic Hello World: Episode 1

--- epicHelloWorld/epichello.py
+++ epicHelloWorld/epichello.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-#Epic Hello  World is an ongoing educational project where we take a simple hello world program and make it better every day. The idea is to teach something new daily and also to encourage daily coding practice
+#Epic Hello world program
 
-hel = 'Hello'
-print (hel,'World!')
+print ('Hello world!')
 
-times = int(input('How many times do you want hello world? \n'))
-
-print ('Hello world\n'*times)
